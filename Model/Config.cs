d6c8b35ae09795308002ca76e1d33a5baccb9818
Goncharov Dmitry <prototype1357@ukr.net>
2021-12-28 13:15:14 +0200

    Added Config class, which contains main preset values

---
+++ Model/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentCalculation.Model
+{
+    public static class Config
+    {
+        public const byte MONTH_WORKING_HOURS_ALLOWED = 160;
+        public const byte DAY_WORKING_HOURS_ALLOWED = 8;
+        public const decimal SUPERVISOR_MONTH_SALARY = 2700;
+        public const decimal SUPERVISOR_MONTH_BONUS = 270;
+    }
+}
