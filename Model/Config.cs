9949b752b36d0ad0a2a7a81e46ba1d3ec1ebb539
Goncharov Dmitry <prototype1357@ukr.net>
2021-12-28 17:28:41 +0200

    Added CalculatePayment method for LocalEmployee class

--- Model/Config.cs
+++ Model/Config.cs
@@ -12,5 +12,6 @@ namespace PaymentCalculation.Model
         public const byte DAY_WORKING_HOURS_ALLOWED = 8;
         public const decimal SUPERVISOR_MONTH_SALARY = 2700;
         public const decimal SUPERVISOR_MONTH_BONUS = 270;
+        public const decimal LOCAL_EMPLOYEE_MONTH_SALARY = 1500;
     }
 }
--- Model/LocalEmployee.cs
+++ Model/LocalEmployee.cs
@@ -15,5 +15,29 @@ namespace PaymentCalculation.Model
             Salary = salary;
             Position = Position.LocalEmployee;
         }
+
+        public decimal CalculatePayment(List<WorkingSession> sessions)
+        {
+            decimal totalPayment = 0;
+            decimal paymentPerHour = Config.LOCAL_EMPLOYEE_MONTH_SALARY / Config.MONTH_WORKING_HOURS_ALLOWED;
+            try 
+            {
+                foreach(WorkingSession session in sessions)
+                {
+                    if(session.Login == Login)
+                    {
+                        if (session.Gap <= Config.DAY_WORKING_HOURS_ALLOWED)
+                            totalPayment += paymentPerHour * session.Gap;
+                        else
+                            totalPayment += paymentPerHour * Config.DAY_WORKING_HOURS_ALLOWED + (session.Gap - Config.DAY_WORKING_HOURS_ALLOWED) * paymentPerHour * 2;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Не удалось посчитать выплату. " + ex.Message);
+            }
+            return totalPayment;
+        }
     }
 }
